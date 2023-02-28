5acda859ab6d502d96869deec291f70c4843f5cb
Rohit <rohitsinha437@gmail.com>
2023-02-28 23:05:59 +0530

    Initial commit

---
+++ Config.py
@@ -0,0 +1,7 @@
+customhost = "emp.cfz3y9onpdfj.us-east-2.rds.amazonaws.com"
+customuser = "rohit"
+custompass = "rohit12sd"
+customdb = "emp"
+custombucket = "myaddemp"
+customregion = "us-east-2"
+customtable = "employee_image_table"
